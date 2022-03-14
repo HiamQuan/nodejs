66a9c24ac103e6976d3fd74407a618bd0313545b
Nguyễn Anh Quân <ngongquan@gmail.com>
2022-03-15 00:02:04 +0700

    feat: post(controller),post(routes)

--- controller/posts.js
+++ controller/posts.js
@@ -1,17 +1,32 @@
 import mongoose from "mongoose";
 
-const Post = mongoose.model("Post",{title: String, desc: String});
+const Post = mongoose.model('post', { name: String, view: Number, desc: String });
 
-const posts = new Post();
+const post = new Post({title:"San pham A",view: 20, desc: "Day la mo ta"});
 
 
-// List post
-export const list = (order,res) => {
-    res.json();
+// List posts
+export const list = async (req, res) => { 
+    res.json(post);
+}
+export const read = (req, res) => {
+    res.json(post.find(item => item.id === +req.params.id));
 }
 
-export const read = (order,res) => {
-    res.json(posts.find((item) => item.id=== order.params.id));
+// API thêm sản phẩm
+export const create = async (req, res) => {
+    try {
+        const post = await new posts(req.body).save();
+        res.json(post)    
+    } catch (error) {
+        res.status(400).json({
+            message: "Not allow to add"
+        })
+    }
 }
-
-// API 
+export const remove = (req, res) => {
+    res.json(post.filter(item => item.id !== +req.params.id));
+}
+export const update = (req, res) => {
+    res.json(post.map(item => item.id == req.params.id ? req.body : item));
+}
